092317a82e07b55b0a06b87f4dfd680ebf86457f
feJustino <fwxjustino@gmail.com>
2023-10-26 21:33:16 -0300

    feat: create next-shop

---
+++ src/styles/index.ts
@@ -0,0 +1,18 @@
+import { createStitches } from "@stitches/react";
+
+export const {
+  config,
+  styled,
+  globalCss,
+  keyframes,
+  getCssText,
+  css,
+  theme,
+  createTheme
+} = createStitches({
+  theme: {
+    colors: {
+      principal: "#00875F"
+    }
+  }
+})
